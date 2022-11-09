25281af4e5b105f503016f932ad56c4d6bcf215e
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-11-09 12:49:10 -0600

    a lot of files

---
+++ Prototype 1/Assets/Scripts/NewPlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayerController : MonoBehaviour
+{1
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // controller input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move Vehicle Forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the vehicle during turns
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+    }
+}
