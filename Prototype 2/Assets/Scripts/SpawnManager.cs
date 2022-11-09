0b425cbb2e2a9a3d76a37e4d0719b476afea596b
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-11-09 13:15:01 -0600

    added spawnmanager variables

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -11,14 +11,17 @@ public class SpawnManager : MonoBehaviour
     }
 
     public GameObject[ ] animalPrefabs;
-    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     // Update is called once per frame
     void Update()
     {
            if(Input.GetKeyDown(KeyCode.S)) {
+           Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0,0,20)), animalPrefabs[animalIndex].transform.rotation;
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation;
 
            }
     }
