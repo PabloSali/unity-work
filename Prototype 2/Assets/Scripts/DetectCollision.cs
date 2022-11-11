ffd7fb3946697df05ff55a22f437b0fafec41291
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-11-11 08:56:15 -0600

    finished animal game

--- Prototype 2/Assets/Scripts/DetectCollision.cs
+++ Prototype 2/Assets/Scripts/DetectCollision.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DetectCollision : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -17,7 +17,7 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other){
-    Destroy(gameObject);
-    Destroy(other.gameObject);
+        Destroy(gameObject);
+        Destroy(other.gameObject);
     }
 }
